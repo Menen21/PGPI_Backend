4fc6df81f1faba5c61af3fb1865d3be5439c84c4
Menen21 <menen210@gmail.com>
2021-03-15 16:06:11 +0100

    First Commit

---
+++ src/main/java/com/logiva/pgpi/PgpiApplication.java
@@ -0,0 +1,13 @@
+package com.logiva.pgpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logiva/pgpi/PgpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiva.pgpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
